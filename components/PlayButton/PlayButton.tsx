2b7f6036692e378000c8420a06e36c69ad0585c3
raph <guest1@mac.mynetworksettings.com>
2025-06-25 17:46:23 -0400

    adjust PlayButton transition CSS

--- components/PlayButton/PlayButton.tsx
+++ components/PlayButton/PlayButton.tsx
@@ -3,7 +3,11 @@ import { FaPlay } from "react-icons/fa";
 import styles from "./PlayButton.module.scss";
 
 const PlayButton = () => {
-  return <FaPlay className={styles.playButtonIcon} />;
+  return (
+    <button className={styles.playButton}>
+      <FaPlay className={styles.playButtonIcon} />
+    </button>
+  );
 };
 
 export default PlayButton;
